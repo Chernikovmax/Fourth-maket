aac05589d71bcaa58f166b03fb51071b9a0fcc55
Chernikovmax <chernik.max@gmail.com>
2018-10-11 16:43:20 +0300

    Have changed colors for button, js code. Now it works fine and looks good.

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const likeBox = document.querySelector('.like-n-meter');
 const likeButton = likeBox.querySelector('.like');
 const likeMeter = likeBox.querySelector('.like-meter');
-const likeStyles = document.querySelector('.like--not-clicked');
 let likeScore = 1;
 
 function likeScoreUp() {
   likeMeter.innerText = ("+" + likeScore++);
 }
 
-function changeClassByClick() {
-  likeStyles.classList.add('like--clicked');
-  likeStyles.classList.remove('like--not-clicked');
+function addClassByClick() {
+  likeButton.classList.add('active');
 }
 
 likeButton.addEventListener('click', likeScoreUp);
-likeButton.addEventListener('click', changeClassByClick);
+likeButton.addEventListener('click', addClassByClick);
