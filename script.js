f6f471324cd233be4d5b67a783c510b524898c57
Chernikovmax <chernik.max@gmail.com>
2018-10-11 15:48:15 +0300

    beta 0.1

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const likeBox = document.querySelector('.like-n-meter');
 const likeButton = likeBox.querySelector('.like');
 const likeMeter = likeBox.querySelector('.like-meter');
+const likeStyles = document.querySelector('.like--not-clicked');
 let likeScore = 1;
 
 function likeScoreUp() {
   likeMeter.innerText = ("+" + likeScore++);
 }
 
+function changeClassByClick() {
+  likeStyles.classList.add('like--clicked');
+  likeStyles.classList.remove('like--not-clicked');
+}
+
 likeButton.addEventListener('click', likeScoreUp);
+likeButton.addEventListener('click', changeClassByClick);
